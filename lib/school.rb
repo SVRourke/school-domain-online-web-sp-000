39f96f38cae4041ba5604c051cc0d8c59fec63f4
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-15 18:19:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,12 +13,12 @@ class School
 
   def add_student(name, grade)
     puts roster.keys
-  #   if roster.keys.include?(grade)
-  #     @roster[:grade] << name
-  #   else
-  #     roster[grade] = []
-  #     @roster[:grade] << name
-  #   end
+    if roster.keys.include?(grade)
+      @roster[:grade] << name
+    else
+      roster[grade] = []
+      @roster[:grade] << name
+    end
   end
 
 end
