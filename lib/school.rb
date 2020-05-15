9c679e38a43c6f7fc4c3b332031669f5d1e55d13
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-15 18:11:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,10 @@
 # code here!
 class School
   attr_reader :name, :roster
-  @roster = Hash.new
 
   def initialize(name)
     @name = name
+    @roster = Hash.new
   end
 
   def add_student(name, grade)
