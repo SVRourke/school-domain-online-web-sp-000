09ad4d8239117801b4a7ce4fea53356f64260f43
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-15 18:27:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,12 +8,13 @@ class School
   end
 
   def add_student(name, grade)
-    if roster.keys.include?(grade.to_sym!)
-      @roster[grade.to_sym] << name
-    else
-      roster[grade.to_sym] = []
-      @roster[grade.to_sym] << name
-    end
+    puts grade.to_sym
+    # if roster.keys.include?(grade.to_sym!)
+    #   @roster[grade.to_sym] << name
+    # else
+    #   roster[grade.to_sym] = []
+    #   @roster[grade.to_sym] << name
+    # end
   end
 end
 
